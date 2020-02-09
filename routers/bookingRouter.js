265da973948c9626b3850860861a6613554efdac
Manoj <malik.manoj35@gmail.com>
2020-02-09 19:41:54 +0530

    added create booking

--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bookingRouter = express.Router();
 
 const {createCheckoutSession, createNewBooking} = require("../controllers/bookingController");
-
+const {isUserAuthorized} = require("../controllers/authController")
+bookingRouter.use(isUserAuthorized);
 bookingRouter.get("/:id", createCheckoutSession)
 bookingRouter.post("/createNewBooking", createNewBooking);
 
